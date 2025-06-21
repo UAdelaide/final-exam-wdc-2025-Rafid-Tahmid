f5bd3d08da0161cfd15edd4f90fcde6e91292fda
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-21 02:38:31 +0000

    [automatic commit] 2025-06-21 02:38:31 UTC 220.245.96.118

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,10 +3,21 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
+  password: '', // Add password field (empty for default MySQL setup)
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// Test the connection
+pool.getConnection()
+  .then(connection => {
+    console.log('Database connected successfully');
+    connection.release();
+  })
+  .catch(error => {
+    console.error('Database connection failed:', error);
+  });
 
 module.exports = pool;
